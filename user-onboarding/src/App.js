e6cf2e6a1d863f9b3ee57e95f8395a22604de93e
Joshua Shaw <joshua@Joshuas-MacBook-Pro.local>
2020-03-19 14:04:16 -0600

    Converted the for I made to Formic form

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withFormik, Form, Field } from 'formik';
 import UserForm from './Form';
+
 
 function App() {
   return (
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,42 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
+import { withFormik, Form, Field } from 'formik';
 
-function UserForm() {
-  const [user, setUser] = useState({ username: "", password: "" });
-
-  const handleChange = event => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(user.username);
-    console.log(user.password);
-  };
-
+function LoginForm() {
   return (
-    <div className="loginForm">
-      {console.log(user)}
-      <form onSubmit={event => handleSubmit(event)}>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            onChange={event => handleChange(event)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="text"
-            name="password"
-            onChange={event => handleChange(event)}
-          />
-        </label>
-        <button>Submit!</button>
-      </form>
-    </div>
+    <Form>
+      <Field type="email" name="email" placeholder="Email" />
+      <Field type="password" name="password" placeholder="Password" />
+      <button>Submit!</button>
+    </Form>
   );
 }
+
+const UserForm = withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(LoginForm);
 
 export default UserForm;
